10c23bfb69367f0cddb9e0ae926432c5071d1904
yuriy <knifeparty4@gmail.com>
2018-02-18 19:59:39 +0300

    finished with algo (not tested yet)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Collections;
@@ -18,12 +19,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         //todo logging if string are wrong or other problems
-        Pattern p = Pattern.compile(".*(вход\\b|выход\\b).+(([0-1][0-9]|2[0-3]):[0-5][0-9]).*");
+        Pattern fileLinePattern = Pattern.compile(".*(вход\\b|выход\\b).+(([0-1][0-9]|2[0-3]):[0-5][0-9]).*");
+        Pattern fileNamePattern = Pattern.compile("(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[012])-((19|20)\\d\\d)");
        // Matcher m = p.matcher("   вход,   09:00-=-");
-        Stream<String> stream = Files.lines(Paths.get("src/main/resources/file1.csv"));
+        Stream<String> stream = Files.lines(Paths.get("src/main/resources/file2.csv"));
         List<TimePair> collect = stream
             .map(s -> {
-                Matcher matcher = p.matcher(s);
+                Matcher matcher = fileLinePattern.matcher(s);
                 Optional<TimePair> pair = matcher.find()
                     ? Optional.of(
                     new TimePair(ActionType.parseActionType(matcher.group(1)),
@@ -37,7 +39,13 @@ public class Main {
 
         Deque<TimePair> stack = new LinkedList<>();
 
-        //todo if out on top ++ if in at the bottom
+        if (collect.get(collect.size() - 1).getAction().equals(ActionType.IN)) {
+            collect.add(new TimePair(ActionType.OUT, LocalTime.MAX));
+        }
+        if (collect.get(collect.size() - 1).getAction().equals(ActionType.OUT)) {
+            collect.add(0, new TimePair(ActionType.IN, LocalTime.MIN));
+        }
+
         stack.push(collect.get(0));
         for (int i = 1; i < collect.size(); i++) {
             if (collect.get(i).getAction() == ActionType.OUT
@@ -51,7 +59,18 @@ public class Main {
             }
         }
 
-        System.out.println(stack);
+        //System.out.println(stack);
+
+        long resultMinutes = 0;
+
+        while (!stack.isEmpty()) {
+            LocalTime out = stack.pop().getTime();
+            LocalTime in = stack.pop().getTime();
+            resultMinutes += Duration.between(in, out).toMinutes();
+        }
+
+        LocalTime res = LocalTime.MIN.plusMinutes(resultMinutes);
+        System.out.println(res.getHour() + " " + res.getMinute());
 
 
     }
